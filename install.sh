9fce9358cb0182d0c71045f263feb34014dcc253
Lucas Saliés Brum <lucas@archlinux.com.br>
2019-01-23 14:10:01 -0300

    Autocommit por Git-Cron: alterações: add 'install.sh'

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+# read -r -d '' logo <<'EOF'
+IFS='' read -r -d '' logo <<'EOF'
+Bem vindo ao script de instalação do:
+                       _           
+ _ __ ___   __ _  __ _(_) ___ ___  
+| '_ ` _ \ / _` |/ _` | |/ __/ _ \ 
+| | | | | | (_| | (_| | | (_| (_) |
+|_| |_| |_|\__,_|\__, |_|\___\___/ 
+                 |___/             
+EOF
+
+echo "$logo"
+
 cache="/tmp/magico-cache"
 
 echo "Instalando dependencias..."
-sudo pacman -Syu jq jshon git --noconfirm 1> /dev/null 2> /dev/null
+sudo pacman -S jq jshon git --noconfirm 1> /dev/null 2> /dev/null
 
 echo "Clonando o magico..."
 [ -d $cache ] && rm -rf $cache 
