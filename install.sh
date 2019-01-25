583f4396d929ef8f5e317f8ada1a0567ec16ac6b
Jose Augusto <joseaugusto.881@outlook.com>
2019-01-25 15:52:57 -0200

    removendo espacos em branco

--- install.sh
+++ install.sh
@@ -14,12 +14,12 @@
 IFS='' read -r -d '' logo <<'EOF'
 
 Bem vindo ao script de instalação do:
-                       _           
- _ __ ___   __ _  __ _(_) ___ ___  
+                       _
+ _ __ ___   __ _  __ _(_) ___ ___
 | '_ ` _ \ / _` |/ _` | |/ __/ _ \ 
 | | | | | | (_| | (_| | | (_| (_) |
-|_| |_| |_|\__,_|\__, |_|\___\___/ 
-                 |___/             
+|_| |_| |_|\__,_|\__, |_|\___\___/
+                 |___/
 EOF
 
 echo -e "\033[00;35m$logo\033[0m"
@@ -30,8 +30,8 @@ echo "Instalando dependencias..."
 sudo pacman -S jq jshon git --noconfirm 1> /dev/null 2> /dev/null
 
 echo "Clonando o magico..."
-[ -d $cache ] && rm -rf $cache 
-mkdir -p $cache 
+[ -d $cache ] && rm -rf $cache
+mkdir -p $cache
 git clone https://aur.archlinux.org/magico $cache
 cd $cache
 
